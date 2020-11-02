4efd0fd4966ca788620c7ab9bc71722c23843f85
MikZarZSG <mikzarzsg@gmail.com>
2020-11-02 11:45:52 +0100

    Pobieranie danych z BD

--- index.php
+++ index.php
@@ -16,5 +16,29 @@
         
         <input type="submit" value="Dodaj komentarz">
     </form>
+    
+<?php
+    //Pobranie danych do połączenia z BD
+    require_once 'dbconn.php';
+
+    //Nawiązanie połączenia z BD
+    $polaczenie = new mysqli($host, $user, $pass, $db);
+
+    if(!$polaczenie) {
+        //Nieudana próba połączenia
+        echo '<span style="color: red;">Nieudana próba połączenia!</span>';
+    } else {
+        //Zapytanie pobierające
+        $sql = "
+        SELECT * 
+        FROM Komentarze 
+        ORDER BY data DESC, id_komentarz";
+        
+        //Wykonanie zapytania
+        
+        //Zamknięcie połączenia z BD
+        $polaczenie->close();
+    }
+?>
 </body>
 </html>
